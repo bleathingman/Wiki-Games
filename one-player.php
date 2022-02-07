fbe7a63a20382d5d54a70481c9468469915680f4
Syko <likkiifauve@gmail.com>
2022-02-07 22:29:11 +0100

    rien

--- one-player.php
+++ one-player.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>1 player</title>
+  <title>Document</title>
 </head>
 <body>
-    <!--Header-->
-    <?php include_once('header.php'); ?>
+<?php include_once('header.php'); ?>
 </body>
 </html>
