b170b0a6552115a7ef616f019140cad9ec5563a7
Matheo Dourneau <matheo.dourneau@gmail.com>
2022-02-07 23:10:51 +0100

    add Cave Crawler page

--- one-player.php
+++ one-player.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>1 player</title>
 </head>
+
 <body>
-    <!--Header-->
-    <?php include_once('header.php'); ?>
+  <!--Header-->
+  <?php include_once('header.php'); ?>
+  <br>
+  <div class="row row-cols-1 row-cols-md-4 g-3">
+    <div class="col">
+      <div class="card">
+        <img src="images/cave-crawler.jpg" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Cave Crawler</h5>
+          <br>
+          <p class="card-text">Cave Crawler is a short, 2D side-scrolling action platformer.
+            Explore caves filled with interesting enemies and fun platforming challenges.</p>
+            <br>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="images/cave-crawler.jpg" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="images/cave-crawler.jpg" class="card-img-top" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
