4ab2343aec5333ce5f939038631608ece885bb6a
Syko <likkiifauve@gmail.com>
2022-02-08 19:37:12 +0100

    add page 404

---
+++ 404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <!--Header-->
+  <?php include_once('header.php'); ?>
+    <div class="container">
+            <!--Erreur 404-->
+        <h2>Oups! Page non trouvée.</h2>
+        <h1>404</h1>
+        <p>Nous ne trouvons pas la page que vous recherchez.</p>
+        <a class="navbar-brand" href="home.php">Aller à la page d'accueil</a>
+    </div>
+</body>
+</html>
