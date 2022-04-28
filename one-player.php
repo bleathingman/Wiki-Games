eb0d548627b84b321555ba858ebbf1f6555d8abd
Matheo <matheo.dourneau@gmail.com>
2022-04-28 19:18:26 +0200

    add new games in one player page

--- one-player.php
+++ one-player.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="styles.css" rel="stylesheet">
   <title>Document</title>
 </head>
 
@@ -15,34 +16,35 @@
   <div class="row row-cols-1 row-cols-md-4 g-3">
     <div class="col">
       <div class="card">
-        <img src="images/cave-crawler.jpg" class="card-img-top" alt="">
+        <img src="images/cave-crawler.jpg" class="card-img-top" alt="Cave Crawler image">
         <div class="card-body">
           <h5 class="card-title">Cave Crawler</h5>
           <br>
           <p class="card-text">Cave Crawler is a short, 2D side-scrolling action platformer.
             Explore caves filled with interesting enemies and fun platforming challenges.</p>
             <br>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="https://store.steampowered.com/app/1865440/Cave_Crawler/" class="btn btn-secondary">Go Steam</a>
         </div>
       </div>
     </div>
     <div class="col">
       <div class="card">
-        <img src="images/cave-crawler.jpg" class="card-img-top" alt="">
+        <img src="images/himno.jpg" class="card-img-top" alt="Himno image">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">Himno</h5>
+          <p class="card-text">Himno is a peaceful, 2D platformer game with an infinite number of beautiful 
+            procedurally generated maps. Take a breath, and relax.</p>
+          <a href="https://store.steampowered.com/app/931690/Himno/" class="btn btn-secondary">Go Steam</a>
         </div>
       </div>
     </div>
     <div class="col">
       <div class="card">
-        <img src="images/cave-crawler.jpg" class="card-img-top" alt="">
+        <img src="images/Yu-Gi-Oh-Duel-Links.jpg" class="card-img-top" alt="Yu-Gi-Oh-Duel-Links image">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">Yu-Gi-Oh-Duel-Links</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" class="btn btn-secondary">Go Steam</a>
         </div>
       </div>
     </div>
